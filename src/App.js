e395863c5056f4a1b7baf237a11a3fd69621567a
scarter31191 <scarter31191@gmaile.com>
2021-10-19 21:19:57 -0400

    completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import * as React from 'react';
 import TodoList from './TodoList';
-
-
-
-
-function Search(){
-  return (
-    <div>
-      <label htmlForm="search">Search: </label>
-      <input id='search' type='text' />
-    </div>
-  )
-}
+import Search from "./Search"
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
     <div className="App">
       <h1> Todo List </h1>
-      <Search />
+      <Search /><br></br>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id='search' type='text' />
+        </div>
+    )
+}
+
+export default Search
